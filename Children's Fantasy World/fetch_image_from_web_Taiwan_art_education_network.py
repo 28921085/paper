07be11666d32c83a1d3c9539385d9d1113847cbe
28921085 <seer.28921085@gmail.com>
2024-11-11 19:59:18 +0800

    圖片流水號版本

--- Children's Fantasy World/fetch_image_from_web_Taiwan_art_education_network.py
+++ Children's Fantasy World/fetch_image_from_web_Taiwan_art_education_network.py
@@ -6,7 +6,7 @@ import re
 
 # 設定目標 URL 模板
 url_template = "https://ed.arte.gov.tw/ch/content/m_result_vlist_1.aspx?PageNo={}&AE_EPTY=視覺藝術&AE_MEDI={}"
-category = "繪畫"
+category = "西畫"
 
 # 設定圖片存放資料夾
 image_folder = f"dataset/台灣藝術教育網/{category}"
@@ -18,8 +18,9 @@ headers = {
 }
 
 # 總頁數
-total_pages = 1000  # 根據您的描述
+total_pages = 200  # 根據您的描述
 
+num = 0
 # 迴圈遍歷每一頁
 for page in range(1, total_pages + 1):
     print(f"正在處理第 {page} 頁...")
@@ -44,21 +45,23 @@ for page in range(1, total_pages + 1):
                 img_url = f"https://ed.arte.gov.tw/{img_src}"
 
             # 移除或替換檔案名稱中的非法字元
-            img_name = re.sub(r'[\/:*?"<>|]', '_', img_alt) + ".jpg"
-            img_path = os.path.join(image_folder, img_name)
+            # img_name = re.sub(r'[\/:*?"<>|]', '_', img_alt) + ".jpg"
+            # img_path = os.path.join(image_folder, img_name)
+            img_path = os.path.join(image_folder,f"{num}.jpg")
 
             try:
                 cnt += 1
                 img_data = requests.get(img_url, headers=headers).content
                 with open(img_path, "wb") as img_file:
                     img_file.write(img_data)
-                print(f"已下載圖片：{img_name}")
+                print(f"已下載圖片：{num}")
+                num+=1
             except Exception as e:
-                print(f"下載圖片 {img_name} 時發生錯誤：{e}")
+                print(f"下載圖片 {num} 時發生錯誤：{e}")
                 
-    if cnt == 0:
-        print("結束爬蟲")
-        break
+    # if cnt == 0:
+    #     print("結束爬蟲")
+    #     break
       
     # 為了避免對伺服器造成過大負擔，下載每頁後休息 1 秒
     time.sleep(1)
